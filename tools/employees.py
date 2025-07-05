0d20639cbc41e847e2a12615532427c7a320a3a6
rohan vishwakarma win10 <rohanvishwakarma685@gmail.com>
2025-07-05 19:29:45 +0530

    Modifying employee_list

--- tools/employees.py
+++ tools/employees.py
@@ -4,10 +4,4 @@ def employee_list(employee_id: str = None) -> dict:
     """
     Return employee data as well as based on employee Id. 
     """
-    if not employee_id:
-        return employees
-    
-    if not isinstance(employees, dict):
-        return {"error": "Internal Data Error"}
-
     return employees.get(employee_id, {"error": "Employee Not Found"})
