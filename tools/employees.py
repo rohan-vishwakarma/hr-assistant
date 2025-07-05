7bbc32c84b0609b4d704f6c8c7ac3cf07c5d8d1d
Rohan Vishwakarma <116492067+rohan-vishwakarma@users.noreply.github.com>
2025-07-05 19:23:48 +0530

    Changes In Employees File (#1)
    * fix
    
    * Adding Employee List
    
    * Employee List

--- tools/employees.py
+++ tools/employees.py
@@ -2,12 +2,12 @@ from data.employees import employees
 
 def employee_list(employee_id: str = None) -> dict:
     """
-    Return employee data as well as based on employee id.
+    Return employee data as well as based on employee Id. 
     """
     if not employee_id:
         return employees
     
     if not isinstance(employees, dict):
-        return {"error": "Internal data error"}
+        return {"error": "Internal Data Error"}
 
-    return employees.get(employee_id, {"error": "Employee not found"})
+    return employees.get(employee_id, {"error": "Employee Not Found"})
